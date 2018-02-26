3bca6ba1cc54ca6bee360a78c1f4ef721ae22b2f
Satkaaktas <Timmy_nitr0@hotmail.com>
2018-02-26 12:08:50 +0100

    ClimbingDEMO

---
+++ Assets/_Core/Scripts/MC_Climb.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MC_Climb : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.tag == "Edge")
+        {
+            Climb_Edge edge = coll.gameObject.GetComponent<Climb_Edge>();
+            edge.Climb(transform);
+        }
+    }
+
+}
