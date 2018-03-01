a201795fe654aa3cb2ff3081566b46fd19fa2a24
MoaLindgren <moa.lindgren@outlook.com>
2018-03-01 16:04:26 +0100

    MainMenu + PauseMenu
    Några ändringar till MenuScript.
    Tillägg av sprites. Samt en PauseMenu.
    
    Saknas: Att spelet pausar. To be continued C:

--- Assets/_Core/Scripts/MenuScript.cs
+++ Assets/_Core/Scripts/MenuScript.cs
@@ -1,6 +1,6 @@
 ﻿
 //Meny-script som hanterar alla menyer och knappar.
-//Skapat av Moa.
+//Skapat av Moa Lindgren.
 
 using System.Collections;
 using System.Collections.Generic;
@@ -9,78 +9,117 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
-    GameObject MainMenu;
-    GameObject LoadMenu;
-    GameObject SettingsMenu;
-    GameObject ConfirmQuit;
-    GameObject CreditsMenu;
-    List<GameObject> Menus;
-    int numberOfSaves;
+    GameObject MainMenu,            //
+               LoadMenu,            //
+               SettingsMenu,        //  <- Huvudmeny
+               ConfirmQuit,         //
+               CreditsMenu,         //
+               Background,          //
 
+               PauseMenu;           //  <- Pausmeny
+
+    [SerializeField]
+    List<GameObject> Menus;
+
+    int numberOfSaves;
+    string gameScene;
+
+    bool paused, 
+         inGame;
+
+    //Spara Canvas till nästa scen.
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    //Sätt alla värden
     void Start()
     {
-        //Blir det problem, along the way, så är det med största sannolikhet följande "satta värden" som ändrats i Unitys hierarki.
-        MainMenu = transform.GetChild(1).gameObject;
-        LoadMenu = transform.GetChild(2).gameObject;
-        SettingsMenu = transform.GetChild(3).gameObject;
-        CreditsMenu = transform.GetChild(4).gameObject;
-        ConfirmQuit = transform.GetChild(5).gameObject;
+        gameScene = "Moa_DemoScene"; //Ändra det här till den färdiga spel-scenen.
+        inGame = false;
+        Menus = new List<GameObject>() { Background, LoadMenu, SettingsMenu, CreditsMenu, ConfirmQuit, MainMenu, PauseMenu };
 
-        Menus = new List<GameObject>() { MainMenu, LoadMenu, SettingsMenu, CreditsMenu, ConfirmQuit };
-        
+        for (int i = 0; i < Menus.Count; i++)
+        {
+            Menus[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    //Öppna och stänga pausmeny.
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && inGame)
+        {
+            paused = !paused;
+
+            if(paused)
+            {
+                Menus[6].SetActive(true);
+            }
+            else
+            {
+                Menus[6].SetActive(false);
+            }
+        }
     }
 
     //Följande metod är kopplat till OnClick() på knapparna i menyn i Unity.
     //Index är specificerat hos vardera knapp i Unity. 
     public void ClickButtons(int index)
     {
-        MainMenu.SetActive(false);
+        Menus[5].SetActive(false);
 
         switch (index)
         {
             //New Game
             case 0:
-                SceneManager.LoadScene("Moa_DemoScene"); // Ändra det här till den färdiga spelscenen
+                inGame = true;
+                Menus[0].SetActive(false);
+                SceneManager.LoadScene(gameScene); 
+
                 break;
 
             //Load Game
             case 1:
-                LoadMenu.SetActive(true);
+
+                Menus[1].SetActive(true);
 
                 //Det här bör ändras till att den hämtar värde från vår spar-funktion.
                 numberOfSaves = 5;
-                for(int i = 0; i < numberOfSaves; i++)
+                for (int i = 0; i < numberOfSaves; i++)
                 {
-                    LoadMenu.transform.GetChild(i).gameObject.SetActive(true);
+                    Menus[1].transform.GetChild(i).gameObject.SetActive(true);
                 }
                 break;
 
             //Settings
             case 2:
-                SettingsMenu.SetActive(true);
+                Menus[2].SetActive(true);
                 break;
 
             //Credits
             case 3:
-                CreditsMenu.SetActive(true);
+                Menus[3].SetActive(true);
                 break;
 
             //Quit
             case 4:
-                ConfirmQuit.SetActive(true);
+                Menus[4].SetActive(true);
                 break;
-            
+
             //Back (till MainMenu)
             case 5:
                 //Funderingar: En if-sats som kollar om man är in-game eller inte. (Do you want to save current game?)
                 //             En till if-sats som kollar om man har sparat eller inte ??? Så det inte dyker upp om man redan har sparat. 
-                for(int i = 0; i < Menus.Count; i++)
+                for (int i = 0; i < Menus.Count; i++)
                 {
                     Menus[i].SetActive(false);
                 }
-                MainMenu.SetActive(true);
+                Menus[0].SetActive(true);
+                Menus[5].SetActive(true);
                 break;
-            
+
             //Yes-knapp i ConfirmQuit.
             case 6:
                 Application.Quit();
