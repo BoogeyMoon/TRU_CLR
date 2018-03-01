44a5c83e695cbfdb28cb4d55df9f0e3e06f741de
Freitas <freitas.adf@hotmail.com>
2018-03-01 12:20:46 +0100

    Camera follow
    Kameran följer med musen när den är nära kanten

--- Assets/_Core/Scripts/CameraManager.cs
+++ Assets/_Core/Scripts/CameraManager.cs
@@ -1,13 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Av Andreas de Freitas och Timmy Alvelöv.
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
 
-public class CameraManager : MonoBehaviour {
-    [SerializeField] Transform player;
-   
-    void Update () {
-        transform.position = Vector3.Lerp(transform.position, player.position, 12f*Time.deltaTime);
-	}
-     
-    
+    int screenHeight, screenWidth;
+
+    [SerializeField]
+    int boundary = 50, speed = 30;
+
+    void Start()
+    {
+        screenHeight = Screen.height;
+        screenWidth = Screen.width;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position, 12f * Time.deltaTime);
+
+        if (Input.mousePosition.x > screenWidth - boundary)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));  //+X Axeln
+        }
+        if (Input.mousePosition.x < 0 + boundary)
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0)); //-X Axeln
+        }
+        if (Input.mousePosition.y > screenHeight - boundary)
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime)); //+Z Axeln
+        }
+        if (Input.mousePosition.y < 0 + boundary)
+        {
+            transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime)); //-Z Axeln
+        }
+    }
+
+
 }
