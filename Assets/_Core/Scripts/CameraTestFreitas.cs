7bcf1a4ced4ba1917b47e3b8af72d8e9a5fc5878
lucepuce <lucas_keno@hotmail.com>
2018-03-01 13:50:19 +0100

    Scenändringar + Camera Test changes
    Ändringar i Lucas Scen + ändrade Freitas Camera script så det blev lit

--- Assets/_Core/Scripts/CameraTestFreitas.cs
+++ Assets/_Core/Scripts/CameraTestFreitas.cs
@@ -10,11 +10,12 @@ public class CameraTestFreitas : MonoBehaviour
     [SerializeField]
     int boundary = 50, speed = 30;
 
-    int screenWidth;
+    int screenHeight, screenWidth;
 
     void Start()
     {
         screenWidth = Screen.width / 2;
+        screenHeight = Screen.height;
     }
 
     void Update()
@@ -28,6 +29,14 @@ public class CameraTestFreitas : MonoBehaviour
         if (Input.mousePosition.x < 0 + boundary)
         {
             transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0)); //-X Axeln
+        }
+        if (Input.mousePosition.y > screenHeight - boundary)
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0)); //+Y Axeln
+        }
+        if (Input.mousePosition.y < 0 + boundary)
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0)); //-Y Axeln
         }
     }
 }
