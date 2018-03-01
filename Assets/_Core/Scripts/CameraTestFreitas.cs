2e39d39086e41e28d62ee70d0e5a7b53d61be68d
Freitas <freitas.adf@hotmail.com>
2018-03-01 13:27:56 +0100

    MouseCamTestFreitas
    Den andra versionen av cameraFollow :penguin:

---
+++ Assets/_Core/Scripts/CameraTestFreitas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTestFreitas : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    [SerializeField]
+    int boundary = 50, speed = 30;
+
+    int screenWidth;
+
+    void Start()
+    {
+        screenWidth = Screen.width / 2;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, player.position, 12f * Time.deltaTime);
+
+        if (Input.mousePosition.x > screenWidth - boundary)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0)); //+X Axeln     
+        }
+        if (Input.mousePosition.x < 0 + boundary)
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0)); //-X Axeln
+        }
+    }
+}
--- Assets/_Core/Scripts/testMCmovement.cs
+++ Assets/_Core/Scripts/testMCmovement.cs
@@ -125,7 +125,5 @@ public class testMCmovement : MonoBehaviour
         {
             moveDirection.y = 0;
         }
-
     }
-
 }
