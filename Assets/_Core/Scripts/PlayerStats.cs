69ecc8fcb89cdf4039cbf48a085db250c1747cb9
Freitas <freitas.adf@hotmail.com>
2018-03-05 15:38:03 +0100

    playerStats

---
+++ Assets/_Core/Scripts/PlayerStats.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//Av Andreas de Freitas och Timmy Alvelöv.
+
+//Håller koll på spelarens hälsa och checkpoints
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField]
+    float maxHealth;
+
+    float health, cPHealth;
+
+    string checkpoint;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetFloat("savedHealth") != -1)
+        {
+            health = cPHealth;
+        }
+    }
+
+    public void ChangeHealth(float value) //Lägg till eller ta bort hälsa från spelaren
+    {
+        health += value;
+
+        if (health >= maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        if (health <= 0)
+        {
+            PlayerDies();
+        }
+    }
+
+    public void PlayerDies() //Ifall spelaren dör
+    {
+        PlayerPrefs.SetFloat("savedHealth", cPHealth);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        health = cPHealth;
+        //  Lastcheckpoint();
+    }
+
+    public void SetCheckpoint(string checkpoint) //Bestämmer checkpointen för spelaren
+    {
+        this.checkpoint = checkpoint;
+        cPHealth = health;
+        
+    }
+}
