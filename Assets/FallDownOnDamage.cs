0ea819599b76a386b57dccb25d94604744a11157
aRcHmaGe333 <slavkostojnic@yahoo.co.uk>
2018-03-06 16:12:34 +0100

    Environment Destroy Demo + Level Sketch update
    Ask me about the environment demo. It's awesome. We all love it.

---
+++ Assets/FallDownOnDamage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDownOnDamage : MonoBehaviour {
+
+
+    private Rigidbody [] bodies;
+    private Collider[] colliders;
+    private Collider mainCollider;
+    private Rigidbody iFall;
+    private Transform[] unparent;
+    void Start()
+        {
+        bodies = GetComponentsInChildren<Rigidbody>();
+        colliders = GetComponentsInChildren<Collider>();
+        mainCollider = gameObject.GetComponent<Collider>();
+        unparent = GetComponentsInChildren<Transform>();
+        }
+    void OnTriggerEnter(Collider other)
+        {
+        mainCollider.enabled = false;
+        Destroy(gameObject, .01f);
+
+        foreach (Transform unpa in unparent)
+        {
+            unpa.transform.SetParent(null, true);
+        }
+        foreach (Collider coll in colliders)
+        {
+            coll.enabled = true;
+        }
+        foreach (Rigidbody body in bodies)
+        {
+            //iFall = GetComponent<Rigidbody>();
+            body.useGravity = true;
+            body.constraints = RigidbodyConstraints.FreezePositionZ;
+            Destroy(body.gameObject, 3f);
+        }
+        
+
+    }
+    }
+
