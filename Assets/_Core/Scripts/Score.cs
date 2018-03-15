17c7bddf581795163b8be08a81e98082e731df63
Freitas <freitas.adf@hotmail.com>
2018-03-15 11:20:07 +0100

    score
    scorefunktion basic af

---
+++ Assets/_Core/Scripts/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//Av Andreas de Freitas
+public class Score : MonoBehaviour
+{
+    [SerializeField]
+    Text displayScore;
+
+    [SerializeField]
+    int currentScore, scoreMultiplyer;
+
+    void Start()
+    {
+        InvokeRepeating("AddScore", 1, 1);
+    }
+
+    void Update()
+    {
+        displayScore.text = "Score: " + currentScore.ToString();
+    }
+
+    void AddScore()
+    {
+        currentScore += scoreMultiplyer;
+    }
+
+}
