b391fe9de00af5d756e776886f9ea70003e77266
Satkaaktas <Timmy_nitr0@hotmail.com>
2018-03-26 12:45:34 +0200

    ... och patrullpunkter

--- Assets/_Core/Scripts/Fiender/Projektiler/Mob_bullet.cs
+++ Assets/_Core/Scripts/Fiender/Projektiler/Mob_bullet.cs
@@ -20,7 +20,7 @@ public class Mob_bullet : Mob_Projectile
 
     void OnTriggerEnter(Collider coll) //Kollar om den kolliderar med något
     {
-        if(coll.gameObject.tag != "Weakpoint" && coll.gameObject.tag != "Bullet") // Ignorerar andra fiender och kulor 
+        if(coll.gameObject.tag != "Weakpoint" && coll.gameObject.tag != "Bullet" && coll.gameObject.tag != "PatrolPoint") // Ignorerar andra fiender, kulor och patrullpunkter 
         {
             if(coll.gameObject.tag == "Player")
             {
