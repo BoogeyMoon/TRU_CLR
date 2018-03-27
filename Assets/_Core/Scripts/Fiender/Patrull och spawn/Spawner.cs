503ca8ca7c633bddd76208bd60d7d230ed1269f8
aRcHmaGe333 <slavkostojnic@yahoo.co.uk>
2018-03-27 15:17:26 +0200

    More awesome changes and updates

--- Assets/_Core/Scripts/Fiender/Patrull och spawn/Spawner.cs
+++ Assets/_Core/Scripts/Fiender/Patrull och spawn/Spawner.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// by Slavko Stojnic
 public class Spawner : MonoBehaviour {
 
     [SerializeField]
@@ -11,21 +11,25 @@ public class Spawner : MonoBehaviour {
     float time;
     [SerializeField]
     Transform spawnPoint;
+    float dist;
+    Transform getPlayer;
+    [SerializeField]
+    float aggroRange;
 
-	// Use this for initialization
-	void Start () {
-        
-		
+    // Use this for initialization
+    void Start ()
+    {
+        getPlayer = GameObject.FindGameObjectWithTag("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        dist = Vector3.Distance(transform.position, getPlayer.position);
         time = time + Time.deltaTime;
-        if (time >= interval)
+        if ((time >= interval) && (dist<=aggroRange))
         {
             Instantiate(spawnThis, spawnPoint.position, spawnThis.transform.rotation);
             time = 0;
-        }
-		
+        }		
 	}
 }
