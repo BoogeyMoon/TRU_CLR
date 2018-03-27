883bb54c06c74bebb8d55c17ea6100bfe7c6e773
lucepuce <lucas_keno@hotmail.com>
2018-03-27 15:38:28 +0200

    Skapat Envorimental Scripts
    Enviromental scrips + ändringar i min scen

---
+++ Assets/_Core/Scripts/Environment/Enviromental.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enviromental : MonoBehaviour {
+
+    protected GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/_Core/Scripts/Environment/HealthPack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPack : Enviromental {
+
+    [SerializeField]
+    protected float healthGain;
+
+    void OnTriggerEnter(Collider coll) //När spelaren träffar spikesen
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            player.GetComponent<PlayerStats>().ChangeHealth(+healthGain); //Spelaren tar skada
+            Destroy(gameObject);
+            
+        }
+    }
+
+
+}
