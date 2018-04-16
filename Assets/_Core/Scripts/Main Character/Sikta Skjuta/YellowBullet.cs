bb35568064dfd4270d8907e60f4ecae2118bd0dd
BoogeyMoon <qvarnstrom.erik@gmail.com>
2018-04-16 22:47:33 +0200

    Ändrade med yellowbullet script, om något går sönder så är det mitt fel ;;

--- Assets/_Core/Scripts/Main Character/Sikta Skjuta/YellowBullet.cs
+++ Assets/_Core/Scripts/Main Character/Sikta Skjuta/YellowBullet.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Skapat av Moa Lindgren. Ärver från Projectile.
+
+//Erik har stökat ner, tog bort så det inte va dubbel dropValue och variablarna inte sattes i start.
+
 //Följande script lägger till en ökad gravity till det gula skottet.
 public class YellowBullet : Projectile
 {
 
-    float dropValue;
+    float dropValue = 0;
     [SerializeField]
-    float gravity;
-    float zOffSet;
+    float gravity = 0.5f;
+    float zOffSet = -0.85f;
     Vector3 position;
 
     void Start()
     {
         base.Start();
-        zOffSet = -0.85f;
-        dropValue = 0;
-        gravity = 0.5f;
     }
     
     void Update()
@@ -26,7 +26,7 @@ public class YellowBullet : Projectile
         base.Update();
         dropValue = dropValue - gravity * Time.deltaTime;
         transform.Translate(Vector3.forward * startVelocity * Time.deltaTime);
-        transform.Translate(new Vector3(0, dropValue, 0), Space.World);
+        //transform.Translate(new Vector3(0, dropValue, 0), Space.World);
         transform.position = new Vector3(transform.position.x, transform.position.y + dropValue, zOffSet);
     }
 
