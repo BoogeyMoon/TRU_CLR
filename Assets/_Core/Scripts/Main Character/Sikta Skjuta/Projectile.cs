28547561acedbeec9bb6fe9a9a6af431bb49a226
Satkaaktas <Timmy_nitr0@hotmail.com>
2018-05-14 12:35:31 +0200

    Sätter starttime i start istället för innan jag dödar den

--- Assets/_Core/Scripts/Main Character/Sikta Skjuta/Projectile.cs
+++ Assets/_Core/Scripts/Main Character/Sikta Skjuta/Projectile.cs
@@ -34,7 +34,6 @@ public class Projectile : MonoBehaviour
             if (startTime >= lifeTime)
             {
                 particle.Stop(true);
-                startTime = 0;
                 _pool.DestroyPool(transform);
             }
         }
