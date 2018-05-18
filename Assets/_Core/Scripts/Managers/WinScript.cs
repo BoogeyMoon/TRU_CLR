afc5f3fdb0e8c4debc33bd36a25e812898d19b43
Erik Qvarnström <qvarnstrom.erik@gmail.com>
2018-05-18 11:07:57 +0200

    Correct Grading fixed on winscreen

--- Assets/_Core/Scripts/Managers/WinScript.cs
+++ Assets/_Core/Scripts/Managers/WinScript.cs
@@ -10,7 +10,6 @@ public class WinScript : MonoBehaviour
     
     [SerializeField]
     List<Transform> winCons;
-    [SerializeField]
     Sprite[] grades;
 
     Score ScoreManager;
@@ -18,6 +17,18 @@ public class WinScript : MonoBehaviour
     GameObject canvas;
     void Start()
     {
+
+        Sprite Pass = Resources.Load("Pass", typeof(Sprite)) as Sprite;
+        Sprite Good = Resources.Load("Good", typeof(Sprite)) as Sprite;
+        Sprite Great = Resources.Load("Great", typeof(Sprite)) as Sprite;
+        Sprite Awesome = Resources.Load("Awesome", typeof(Sprite)) as Sprite;
+        Sprite TRUCLR = Resources.Load("TRU CLR", typeof(Sprite)) as Sprite;
+
+        print(Pass.name);
+
+        grades = new Sprite[] { Pass, Good, Great, Awesome, TRUCLR };
+
+
         canvas = GameObject.FindGameObjectWithTag("MenuCanvas").gameObject;
         ScoreManager = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<Score>();
         winConditions = new bool[winCons.Count];
@@ -40,14 +51,16 @@ public class WinScript : MonoBehaviour
         yield return new WaitForSeconds(1.5f);
         canvas.transform.GetChild(7).gameObject.SetActive(true);
         Text levelName = canvas.transform.GetChild(7).GetChild(0).GetChild(0).GetComponent<Text>();
-        levelName.text = "memes";
+        levelName.text = "Level";
         Text baseScore = canvas.transform.GetChild(7).GetChild(0).GetChild(3).GetComponent<Text>();
         baseScore.text = ScoreManager.CurrentScore.ToString();
         Text totalScore = canvas.transform.GetChild(7).GetChild(0).GetChild(5).GetComponent<Text>();
         int grade = ScoreManager.GetGrade();
         totalScore.text = ScoreManager.CurrentScore.ToString();
         Image rating = canvas.transform.GetChild(7).GetChild(0).GetChild(7).GetComponent<Image>();
-        rating.sprite = grades[grade];
+        print(grade);
+        print(grades.Length);
+        rating.sprite = grades[grade - 1];
         Time.timeScale = 0;
     }
 
