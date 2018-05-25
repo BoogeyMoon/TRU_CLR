c248409e389ab7c7c03673a84782032ab5dba73a
Satkaaktas <Timmy_nitr0@hotmail.com>
2018-05-25 10:01:48 +0200

    Homingskotten fuckar inte längre

--- Assets/_Core/Scripts/Enemies/FeedTheMC.cs
+++ Assets/_Core/Scripts/Enemies/FeedTheMC.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 // by Slavko Stojnic
 // Script can be used both for the "elements" that mobs leave behind them and for homing missiles
 
-public class FeedTheMC : MonoBehaviour {
+public class FeedTheMC : MonoBehaviour
+{
 
     private Transform wheresMC;
     private float dist;
@@ -12,24 +13,30 @@ public class FeedTheMC : MonoBehaviour {
     bool doIdestroy;
     [SerializeField]
     float speed;
+    Mob_bullet homing;
 
-	// Use this for initialization
-	void Start () {
-        wheresMC = GameObject.FindGameObjectWithTag("ShootHere").transform;
-    }
-	
-	// Update is called once per frame
-	void Update ()
+    // Use this for initialization
+    void Start()
     {
-        if (transform.position.z != wheresMC.position.z)
-            transform.position = new Vector3(transform.position.x, transform.position.y, wheresMC.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, wheresMC.transform.position, speed*Time.deltaTime);
-        if ((Vector3.Distance(wheresMC.position, transform.position) <=.3f) && doIdestroy)
-        {
-            Destroy(gameObject, 1);
-        }
+        wheresMC = GameObject.FindGameObjectWithTag("ShootHere").transform;
+        if (GetComponent<Mob_bullet>() != null)
+            homing = GetComponent<Mob_bullet>();
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (homing.Active)
+        {
+            if (transform.position.z != wheresMC.position.z)
+                transform.position = new Vector3(transform.position.x, transform.position.y, wheresMC.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, wheresMC.transform.position, speed * Time.deltaTime);
+            if ((Vector3.Distance(wheresMC.position, transform.position) <= .3f) && doIdestroy)
+            {
+                Destroy(gameObject, 1);
+            }
+        }
+    }
 
 
 }
